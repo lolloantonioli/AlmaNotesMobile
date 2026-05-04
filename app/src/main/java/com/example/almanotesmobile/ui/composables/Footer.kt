d04d1111e325be3602d6a12cc12324732ac1d0e0
Lorenzo Antonioli <lorenzo.antonioli2@studio.unibo.it>
2026-05-04 15:56:26 +0200

    Modified navbar

--- app/src/main/java/com/example/almanotesmobile/ui/composables/Footer.kt
+++ app/src/main/java/com/example/almanotesmobile/ui/composables/Footer.kt
@@ -1,33 +1,36 @@
 package com.example.almanotesmobile.ui.composables
 
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarDefaults
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import com.example.almanotesmobile.R
-import com.google.android.material.navigation.NavigationBarView
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
 
 @Composable
 fun NavigationBar() {
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val items = listOf("Home", "Badge", "Profilo")
+    val icons = listOf(Icons.Filled.Home, Icons.Filled.Star, Icons.Filled.Person)
 
-    NavigationBarView.OnItemSelectedListener { item ->
-        when(item.itemId) {
-            R.id.item_1 -> {
-                // Respond to navigation item 1 click
-                true
+    NavigationBar {
+        NavigationBar(windowInsets = NavigationBarDefaults.windowInsets) {
+            items.forEachIndexed { index, item ->
+                NavigationBarItem(
+                    icon = { Icon(icons[index], contentDescription = item) },
+                    label = { Text(item) },
+                    selected = selectedItem == index,
+                    onClick = { selectedItem = index }
+                )
             }
-            R.id.item_2 -> {
-                // Respond to navigation item 2 click
-                true
-            }
-            R.id.item_3 ->{
-                true
-            }
-            R.id.item_4->{
-                true
-            }
-            R.id.item_5 ->{
-                true
-            }
-            else -> false
         }
     }
-
 }
